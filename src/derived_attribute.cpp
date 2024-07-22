ee29d89600c1a622f9198899b2620382ee79f909
OctoD <iamoctod@gmail.com>
2024-07-22 17:30:39 +0200

    chore: drafts runtime derived attribute

--- src/derived_attribute.cpp
+++ src/derived_attribute.cpp
@@ -7,27 +7,45 @@ using namespace gga;
 void DerivedAttribute::_bind_methods()
 {
 	/// binds virtual methods to godot
-	PropertyInfo argument = PropertyInfo(Variant::OBJECT, "attribute_container");
-	Vector<StringName> args = Vector<StringName>({ "attribute_container" });
+	PropertyInfo argument = PropertyInfo(Variant::OBJECT, "attribute_set", PROPERTY_HINT_RESOURCE_TYPE, "AttributeSet");
+	Vector<StringName> args = Vector<StringName>({ "attribute_set" });
 
-	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_initial_value"), args);
-	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_max_value"), args);
-	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_min_value"), args);
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_initial_value", argument), args);
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_max_value", argument), args);
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::FLOAT, "get_min_value", argument), args);
 }
 
-float DerivedAttribute::get_initial_value(const Ref<AttributeContainer> &p_attribute_container) const
+float DerivedAttribute::get_initial_value(const Ref<AttributeSet> &p_attribute_container) const
 {
 	return 0.0f;
 }
 
-float DerivedAttribute::get_max_value(const Ref<AttributeContainer> &p_attribute_container) const
+float DerivedAttribute::get_max_value(const Ref<AttributeSet> &p_attribute_container) const
 {
 	return 0.0f;
 }
 
-float DerivedAttribute::get_min_value(const Ref<AttributeContainer> &p_attribute_container) const
+float DerivedAttribute::get_min_value(const Ref<AttributeSet> &p_attribute_container) const
 {
 	return 0.0f;
+}
+
+#pragma endregion
+
+#pragma region RuntimeDerivedAttribute
+
+void gga::RuntimeDerivedAttribute::_bind_methods()
+{
+}
+
+Ref<RuntimeDerivedAttribute> gga::RuntimeDerivedAttribute::from(const Ref<DerivedAttribute> &p_derived_attribute)
+{
+	return Ref<RuntimeDerivedAttribute>();
+}
+
+Ref<DerivedAttribute> gga::RuntimeDerivedAttribute::to(const Ref<RuntimeDerivedAttribute> &p_runtime_derived_attribute)
+{
+	return Ref<DerivedAttribute>();
 }
 
 #pragma endregion
--- src/derived_attribute.hpp
+++ src/derived_attribute.hpp
@@ -4,13 +4,12 @@
 #include <godot_cpp/classes/resource.hpp>
 
 #include "attribute.hpp"
-#include "attribute_container.hpp"
 
 using namespace godot;
 
 namespace gga
 {
-
+	/// @brief A derived attribute is an attribute that is calculated based on other attributes given by the AttributeSet.
 	class DerivedAttribute : public AttributeBase
 	{
 		GDCLASS(DerivedAttribute, AttributeBase);
@@ -22,13 +21,28 @@ namespace gga
 	public:
 		/// @brief Get the initial value of the attribute.
 		/// @return The initial value of the attribute.
-		virtual float get_initial_value(const Ref<AttributeContainer> &p_attribute_container) const;
+		virtual float get_initial_value(const Ref<AttributeSet> &p_attribute_container) const;
 		/// @brief Get the maximum value of the attribute.
 		/// @return The maximum value of the attribute.
-		virtual float get_max_value(const Ref<AttributeContainer> &p_attribute_container) const;
+		virtual float get_max_value(const Ref<AttributeSet> &p_attribute_container) const;
 		/// @brief Get the minimum value of the attribute.
 		/// @return The minimum value of the attribute.
-		virtual float get_min_value(const Ref<AttributeContainer> &p_attribute_container) const;
+		virtual float get_min_value(const Ref<AttributeSet> &p_attribute_container) const;
+	};
+
+	class RuntimeDerivedAttribute : public DerivedAttribute
+	{
+		GDCLASS(RuntimeDerivedAttribute, DerivedAttribute);
+
+	protected:
+		/// @brief Bind methods to Godot.
+		static void _bind_methods();
+
+		Ref<DerivedAttribute> derived_attribute;
+
+	public:
+		static Ref<RuntimeDerivedAttribute> from(const Ref<DerivedAttribute> &p_derived_attribute);
+		static Ref<DerivedAttribute> to(const Ref<RuntimeDerivedAttribute> &p_runtime_derived_attribute);
 	};
 } //namespace gga
 
